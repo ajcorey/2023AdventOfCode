4a4b45313ba06c9d1e6c40382399cab04de69f6a
ajcorey <alexandercorey662@gmail.com>
2023-12-02 08:58:23 -0500

    day one complete

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -2,9 +2,33 @@ import re
 
 def main():
     calDoc = open("puzzleInput.txt","r")
+    # calDoc = open("subset.txt","r")
     sum = 0
     for line in calDoc:
-        nums = re.findall("\d",line)
+        nums = re.findall(r'(?=(\d|zero|one|two|three|four|five|six|seven|eight|nine))',line)
+        for i in range(len(nums)):
+            match nums[i]:
+                case "zero":
+                    nums[i]="0"
+                case "one":
+                    nums[i]="1"
+                case "two":
+                    nums[i]="2"
+                case "three":
+                    nums[i]="3"
+                case "four":
+                    nums[i]="4"
+                case "five":
+                    nums[i]="5"
+                case "six":
+                    nums[i]="6"
+                case "seven":
+                    nums[i]="7"                    
+                case "eight":
+                    nums[i]="8"
+                case "nine":
+                    nums[i]="9"
+
         sum += (int)(nums[0]+nums[-1])
     print(sum)
 
