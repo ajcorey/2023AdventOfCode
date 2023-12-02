a6af1dda593b2061854e29d9bc47a2bf867cf37b
ajcorey <alexandercorey662@gmail.com>
2023-12-02 11:09:22 -0500

    day2 part 2 complete

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -4,16 +4,22 @@ def main():
     gameList = open("puzzleInput.txt","r")
     # gameList = open("subset.txt","r")
 
-    redLimit = 12
-    greenLimit = 13
-    blueLimit = 14
+    #for part 1
+    # redLimit = 12
+    # greenLimit = 13
+    # blueLimit = 14
 
-    possibleSum = 0
+    # possibleSum = 0
+    powerSum = 0
     for game in gameList:
         gBreakdown = re.split(r":|;",game)
         gameNum = int(re.split(r" ",gBreakdown[0])[1])
 
-        possibleRound = True
+        # possibleRound = True
+        redMax = 0
+        greenMax = 0
+        blueMax = 0
+
         for round in range(1,len(gBreakdown)):
             color = re.split(r",",gBreakdown[round])
             redNum = 0
@@ -28,12 +34,20 @@ def main():
                 elif "blue" in c:
                     blueNum=int(re.split(r" ",c)[1])
 
-            possibleRound = possibleRound and (redNum <= redLimit and greenNum <= greenLimit and blueNum <= blueLimit)
+            # possibleRound = possibleRound and (redNum <= redLimit and greenNum <= greenLimit and blueNum <= blueLimit)
+            redMax = max(redMax,redNum)
+            greenMax = max(greenMax,greenNum)
+            blueMax = max(blueMax,blueNum)
 
-        if possibleRound:
-            possibleSum += gameNum
+        gamePower = redMax * greenMax * blueMax
 
-    print(possibleSum)
+        powerSum += gamePower   
+
+        # if possibleRound:
+            # possibleSum += gameNum
+
+    # print(possibleSum)
+    print(powerSum)
     gameList.close()
 if __name__ == "__main__":
     main()
