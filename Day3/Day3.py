0b4901a84e2625fb37334906d606f3b221db5ada
ajcorey <alexandercorey662@gmail.com>
2023-12-03 02:02:46 -0500

    day3 part 2 finished - shouldnt have stayed up at 0202

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -28,9 +28,10 @@ def main():
     i = 0
     for i in range(len(rowArr)):
         print(rowArr[i])
-        for sym in re.finditer(r"[^\d|\.|\n]",rowArr[i]):
+        for sym in re.finditer(r"[\*]",rowArr[i]):
             if sym is not None:
                 s = symbol(sym.start(),i)
+                print("g")
                 symList.append(s)
 
         for num in re.finditer(r"(\d+)",rowArr[i]):
@@ -40,24 +41,53 @@ def main():
         
         i += 1  
 
-    j=0
-    for j in range(len(numlist)): 
-        # print(numlist[j].val)
-        k=0
-        for k in range(len(symList)):
-            if (numlist[j].row-1 <= symList[k].row) and \
-                (numlist[j].row+1 >= symList[k].row) and \
-                (numlist[j].s_col-1 <= symList[k].col) and \
-                (numlist[j].e_col >= symList[k].col):
-                    # print("col") 
+    #part 1
 
-                print(numlist[j].val)
-                totalSum += int(numlist[j].val)
-                break
+    # j=0
+    # for j in range(len(numlist)): 
+    #     # print(numlist[j].val)
+    #     k=0
+    #     for k in range(len(symList)):
+    #         if (numlist[j].row-1 <= symList[k].row) and \
+    #             (numlist[j].row+1 >= symList[k].row) and \
+    #             (numlist[j].s_col-1 <= symList[k].col) and \
+    #             (numlist[j].e_col >= symList[k].col):
+    #                 # print("col") 
+
+    #             print(numlist[j].val)
+    #             totalSum += int(numlist[j].val)
+    #             break
+
+    #         k += 1
+
+    #     j += 1
+
+    
+    j=0
+    for j in range(len(symList)):
+        k=0
+        cnt = 0
+        r_arr = []
+        for k in range(len(numlist)):
+            if (numlist[k].row-1 <= symList[j].row) and \
+                (numlist[k].row+1 >= symList[j].row) and \
+                (numlist[k].s_col-1 <= symList[j].col) and \
+                (numlist[k].e_col >= symList[j].col):
+                cnt += 1  
+                r_arr.append(numlist[k])       
 
             k += 1
+        if cnt == 2:
+            m=int(r_arr[0].val)*int(r_arr[1].val)
+            totalSum += m
+            r_arr = []
+            cnt=0
+        else:
+            r_arr = []
+            cnt=0
 
         j += 1
+
 
     print(totalSum)
     engineSN.close()
